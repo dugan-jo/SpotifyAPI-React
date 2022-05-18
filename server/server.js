020c022a9a84a2544d2ce82865e6eca4633dc2d9
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-05-17 18:54:08 -0600

    set up Spotify Auth

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectURI: "http://localhost:3000",
+    clientId: "f7135ecc7e824cedbef7b757b8d1f358",
+    clientSecret: "5b3e46e0660a4430a8c8612a5cc29ed0",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
