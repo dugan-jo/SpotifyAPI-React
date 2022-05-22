46be3028669b5378719a6e23f18a63296a72be50
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-05-22 17:55:38 -0600

    nav bar/search bar completed

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import { StateProvider } from "./utils/StateProvider";
 import reducer, { initialState } from "./utils/reducer";
 
