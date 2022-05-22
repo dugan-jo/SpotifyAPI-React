040b646f49ff7fdf26e16d4733fdcfcaf6cbf271
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-05-22 10:51:30 -0600

    created components

--- server/server.js
+++ server/server.js
@@ -1,12 +1,37 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.params.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "f7135ecc7e824cedbef7b757b8d1f358",
+    clientSecret: "5b3e46e0660a4430a8c8612a5cc29ed0",
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      //   console.log(data.body);
+      //   console.log("The access token has been refreshed");
+      spotifyApi.refreshAccessToken(data.body["access_token"]);
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
-    redirectURI: "http://localhost:3000",
+    redirectUri: "http://localhost:3000",
     clientId: "f7135ecc7e824cedbef7b757b8d1f358",
     clientSecret: "5b3e46e0660a4430a8c8612a5cc29ed0",
   });
@@ -20,7 +45,7 @@ app.post("/login", (req, res) => {
         expiresIn: data.body.expires_in,
       });
     })
-    .catch(() => {
+    .catch(err => {
       res.sendStatus(400);
     });
 });
