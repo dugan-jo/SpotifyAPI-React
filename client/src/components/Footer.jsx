1fdd0f19a66e849f8e9080aa3b0c890ccd97f8eb
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-05-22 11:39:31 -0600

    set up styling

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from "styled-components"
+
+export default function Footer() {
+    return <Container>Footer</Container>
+}
+
+const Container = styled.div`
+background-color: #202020;
+height: 100%;
+`;
