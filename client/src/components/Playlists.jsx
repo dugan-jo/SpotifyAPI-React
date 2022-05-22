43bd0303a16497822356fdfad2ef7629e5382cf6
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-05-22 12:07:36 -0600

    user playlists

--- client/src/components/Playlists.jsx
+++ client/src/components/Playlists.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useStateProvider } from '../utils/StateProvider'
+import axios from 'axios'
+import { reducerCases } from '../utils/Constants';
+
 
 export default function Playlists() {
+    const [{ token, playlists }, dispatch] = useStateProvider();
+    useEffect(() => {
+        const getPlaylistData = async () => {
+            const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
+                headers: {
+                    Authorization: "Bearer " + token,
+                    "content-type": "application/json"
+
+                }
+            })
+            const {items} = response.data
+            console.log(items)
+            const playlists = items.map(({name, id}) => {
+                return { name, id };
+            })
+            console.log(playlists);
+            dispatch({ type: reducerCases.SET_PLAYLIST, playlists })
+        };
+        getPlaylistData()
+    }, [token, dispatch])
     return (
-        <div>Playlists</div>
+        <div>
+            <ul>
+                {
+                    playlists.map(({name, id}) => {
+                        return (
+                            <li key={id}></li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
     )
 }
