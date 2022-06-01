de660ef6ffcd2ca6e055a521231c4b24386ad34e
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-06-01 17:41:12 -0600

    got previous and next working

--- client/src/components/PlayerControls.jsx
+++ client/src/components/PlayerControls.jsx
@@ -8,23 +8,58 @@ import {
 import { CgPlayTrackNext, CgPlayTrackPrev } from "react-icons/cg";
 import { FiRepeat } from "react-icons/fi";
 import { useStateProvider } from "../utils/StateProvider";
+import axios from "axios";
+import { reducerCases } from "../utils/Constants";
 
 export default function PlayerControls() {
   const [{ token, playerState }, dispatch] = useStateProvider();
 
+  const changeTrack = async type => {
+    await axios.post(
+      `https://api.spotify.com/v1/me/player/${type}`,
+      {},
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const response = await axios.get(
+      "https://api.spotify.com/v1/me/player/currently-playing",
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(response);
+    if (response.data !== "") {
+      const { item } = response.data;
+      console.log(item);
+      const currentlyPlaying = {
+        id: item.id,
+        name: item.name,
+        artist: item.artists.map(artist => artist.name),
+        image: item.album.images[2].url,
+      };
+      dispatch({ type: reducerCases.SET_PLAYING, currentlyPlaying });
+    } else dispatch({ type: reducerCases.SET_PLAYING, currentlyPlaying: null });
+  };
   return (
     <Container>
       <div className="shuffle">
         <BsShuffle />
       </div>
       <div className="previous">
-        <CgPlayTrackPrev />
+        <CgPlayTrackPrev onClick={() => changeTrack("previous")} />
       </div>
       <div className="state">
         {playerState ? <BsFillPauseCircleFill /> : <BsFillPlayCircleFill />}
       </div>
       <div className="next">
-        <CgPlayTrackNext />
+        <CgPlayTrackNext onClick={() => changeTrack("next")} />
       </div>
       <div className="repeat">
         <FiRepeat />
